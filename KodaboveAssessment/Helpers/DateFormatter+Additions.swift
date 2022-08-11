f513046159eec4975b2b67875567a78d0d9b1188
Drew Barnes <drewbrns@gmail.com>
2022-08-11 23:21:29 +0000

    add remote item loader

---
+++ KodaboveAssessment/Helpers/DateFormatter+Additions.swift
@@ -0,0 +1,19 @@
+//
+//  DateFormatter+Additions.swift
+//  KodaboveAssessment
+//
+//  Created by Drew Barnes on 11/08/2022.
+//
+
+import Foundation
+
+extension DateFormatter {
+    static let iso8601Full: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+        formatter.calendar = Calendar(identifier: .iso8601)
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        return formatter
+    }()
+}
