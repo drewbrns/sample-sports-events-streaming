8eccad725f3b14f6a896dc2da93f8a6eefe9a938
Drew Barnes <drewbrns@gmail.com>
2022-08-12 00:23:46 +0000

    ensure tableview is properly configured when event list vc is loaded

--- KodaboveAssessment/UI/Events/EventListViewController.swift
+++ KodaboveAssessment/UI/Events/EventListViewController.swift
@@ -6,23 +6,34 @@
 //
 
 import UIKit
+import Combine
 
 class EventListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    var vm: ItemList?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension EventListViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
     }
-    */
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+}
+
+extension EventListViewController: UITableViewDelegate {
 
 }
--- KodaboveAssessmentTests/EventListControllerTests.swift
+++ KodaboveAssessmentTests/EventListControllerTests.swift
@@ -10,16 +10,74 @@ import XCTest
 
 class EventListControllerTests: XCTestCase {
 
+    let items = [
+        Item(
+            id: "1",
+            title: "Chelsea vs Manchester Utd",
+            subTitle: "EPL",
+            date: .distantFuture,
+            imageURL: URL(string: "https://via.placeholder.com/150")!
+        ),
+        Item(
+            id: "2",
+            title: "Manchester Utd vs Juventus",
+            subTitle: "Champions League",
+            date: Date(),
+            imageURL: URL(string: "https://via.placeholder.com/150")!
+        ),
+        Item(
+            id: "3",
+            title: "Arsenal vs Ajax",
+            subTitle: "Champions League",
+            date: .distantPast,
+            imageURL: URL(string: "https://via.placeholder.com/150")!
+        )
+    ]
+
     func test_init() {
-        XCTFail()
+        let sut = makeSut()
+
+        XCTAssertNotNil(sut.tableView)
+        XCTAssertNotNil(sut.tableView.delegate)
+        XCTAssertNotNil(sut.tableView.dataSource)
+        XCTAssertNotNil(sut.vm)
     }
 
     // MARK: Helpers
 
-    func makeSut() -> EventListViewController {
+    func makeSut(items: [Item] = []) -> EventListViewController {
+
+        let vm = ItemListViewModel(items: items)
+
         let sut = AppStoryboard.main.viewController(viewControllerClass: EventListViewController.self)
         _ = sut.view
+        sut.vm = vm
         return sut
+    }
+
+    final class ItemListViewModel: ItemList {
+        private var items = [Item]()
+
+        var isEmpty: Bool {
+            return items.isEmpty
+        }
+        var totalCount: Int {
+            return items.count
+        }
+        var currentCount: Int {
+            return items.count
+        }
+
+        init(items: [Item]) {
+            self.items = items
+        }
+
+        func loadData(limit: Int) {
+        }
+
+        func viewModel(at index: Int) -> ItemViewModel {
+            ItemViewModel(item: self.items[index])
+        }
     }
 
 }
